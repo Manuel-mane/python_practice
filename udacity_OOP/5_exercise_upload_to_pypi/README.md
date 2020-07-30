29837168543d95c80ecb054640e21fa6c4982e14
Manuel-mane <manuel.mane093@gmail.com>
2020-07-30 14:43:41 +0000

    OOP: AWS Machine Learning Fundamentals from Udacity

---
+++ udacity_OOP/5_exercise_upload_to_pypi/README.md
@@ -0,0 +1,4 @@
+# Udacity OOP
+
+This package is part of the Udacity Machine Learning Foundation course. 
+In this package we describe the Binomial and Gaussian distributions. 
---
+++ udacity_OOP/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+
+setup(name='dsnd_distributions_manu95',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=setuptools.find_packages(), 
+      author="Manuel Mane",
+      author_email='manito920514@gmail.com',
+      zip_safe=False)
